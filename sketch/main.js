88fd52d5b02d3e5c925695150e3d84e887ff7985
MonikaKoprowska <monika.koprowska@gmail.com>
2018-10-13 03:50:13 +0200

    Update main.js

--- sketch/main.js
+++ sketch/main.js
@@ -240,10 +240,7 @@ allow drawing on canvas
 */
 function allowDrawing() {
     canvas.isDrawingMode = 1;
-    if (mode == 'en')
-        document.getElementById('status').innerHTML = 'Model Loaded';
-    else
-        document.getElementById('status').innerHTML = 'تم التحميل';
+    document.getElementById('status').innerHTML = 'Model Loaded';
     $('button').prop('disabled', false);
     var slider = document.getElementById('myRange');
     slider.oninput = function() {
